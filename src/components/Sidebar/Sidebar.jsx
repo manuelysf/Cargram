0c47ebfafb1f92292ac4f9ae20fe7f123ff13e94
manuelysf <158148948+manuelysf@users.noreply.github.com>
2024-10-11 16:00:01 +0200

    Sidebar basics finished

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,35 @@
-import { Box, Flex, Link } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Link, Tooltip } from '@chakra-ui/react'
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
-import { CargramLogo, CargramLogoMobile } from '../../assets/constants'
+import { CargramLogo, CargramLogoMobile, CreatePostLogo, NotificationsLogo, SearchLogo } from '../../assets/constants'
+import { AiFillHome } from 'react-icons/ai'
+import { BiLogOut } from 'react-icons/bi'
 
 const Sidebar = () => {
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
+    },
+    {
+      icon: <SearchLogo/>,
+      text: "Search",
+    },
+    {
+      icon: <NotificationsLogo/>,
+      text: "Notifications",
+    },
+    {
+      icon: <CreatePostLogo/>,
+      text: "Create",
+    },
+    {
+      icon: <Avatar size={"sm"} name='Manuel Youssef' src='/profilepic.png'/>,
+      text: "Profile",
+      link: "/asaprogrammer",
+    },
+  ]
   return (
     <Box
        height={"100vh"}
@@ -19,21 +45,75 @@ const Sidebar = () => {
              {/* This way we can add Chakra Sytles to the Link */}
              <Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "block" }} cursor='pointer'>
 				<CargramLogo/>
-			</Link>
-            <Link
-					to={"/"}
-					as={RouterLink}
-					p={2}
-					display={{ base: "block", md: "none" }}
-					borderRadius={6}
-					_hover={{
-						bg: "whiteAlpha.200",
-					}}
-					w={10}
-					cursor='pointer'
-				>
-					<CargramLogoMobile/>
-				</Link>
+          </Link>
+                <Link
+              to={"/"}
+              as={RouterLink}
+              p={2}
+              display={{ base: "block", md: "none" }}
+              borderRadius={6}
+              _hover={{
+                bg: "whiteAlpha.200",
+              }}
+              w={10}
+              cursor='pointer'
+            >
+              <CargramLogoMobile/>
+            </Link>
+            <Flex direction={"column"} gap={5} cursor={"pointer"}>
+                {sidebarItems.map((item, index) => (
+                  <Tooltip 
+                  key={index}
+                  hasArrow
+                  label={item.text}
+                  placement='right'
+                  ml={1}
+                  openDelay={500}
+                  display={{base:"block",md:"none"}}
+                  >
+                      <Link
+                      display={"flex"}
+                      to={item.link || null}
+                      as={RouterLink}
+                      alignItems={"center"}
+                      gap={4}
+                      _hover={{bg:"whiteAlpha.400"}}
+                      borderRadius={6}
+                      p={2}
+                      w={{base:10,md:"full"}}
+                      justifyContent={{ base: "center", md: "flex-start" }}
+                      >
+                        {item.icon}
+                        <Box display={{base:"none",md:"block"}}>{item.text}</Box>
+                      </Link>
+                  </Tooltip>
+                ))}
+            </Flex>
+              <Tooltip
+                  hasArrow
+                  label={"Logout"}
+                  placement='right'
+                  ml={1}
+                  openDelay={500}
+                  display={{base:"block",md:"none"}}
+                  >
+                      <Link
+                      display={"flex"}
+                      to={"/auth"}
+                      as={RouterLink}
+                      alignItems={"center"}
+                      gap={4}
+                      _hover={{bg:"whiteAlpha.400"}}
+                      borderRadius={6}
+                      p={2}
+                      w={{base:10,md:"full"}}
+                      mt={"auto"}
+                      justifyContent={{ base: "center", md: "flex-start" }}
+                      >
+                        <BiLogOut size={25}/>
+                        <Box display={{base:"none",md:"block"}}>Logout</Box>
+                      </Link>
+                </Tooltip>
         </Flex>
     </Box>
   )
