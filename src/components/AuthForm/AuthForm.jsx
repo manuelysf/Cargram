f3b4c2dbc0db8e7f24f6cfb8dd8de013eb5ff9e4
manuelysf <158148948+manuelysf@users.noreply.github.com>
2024-10-15 19:07:37 +0200

    AuthForm seperated and implemented Singup with email and password v1

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,56 +1,19 @@
-import { Box, VStack, Image, Input, Button, Flex, Text } from '@chakra-ui/react'
+import { Box, VStack, Image, Flex, Text } from '@chakra-ui/react'
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import Login from './Login'
+import Signup from './Signup'
+import GoogleAuth from './GoogleAuth'
 
 const AuthForm = () => {
     const [isLogin, setIsLogin] = useState(true)
-    const navigate = useNavigate();
-    const [inputs, setInputs] = useState({
-        email:'',
-        password:'',
-        confirmPassword:''
-    });
-    const handleAuth = () => {
-        if(!inputs.email || !inputs.password){
-            alert("Please fill all the fields");
-            return;
-        }
-
-        navigate("/");
-    }
+ 
   return (
     <>
         <Box border={"1px solid gray"} borderRadius={4} padding={5}>
             <VStack spacing={4}>
                 <Image src='/logo.png' h={24} cursor={"pointer"} alt='Cargram'/>
-                <Input
-                    placeholder='Email'
-                    fontSize={14}
-                    type='email'
-                    value={inputs.email}
-                    onChange={(e) => setInputs({...inputs,email:e.target.value})}
-                />
-                <Input
-                    placeholder='Password'
-                    fontSize={14}
-                    type='password'
-                    value={inputs.password}
-                    onChange={(e) => setInputs({...inputs,password:e.target.value})}
-                />
 
-                {!isLogin ? (
-                    <Input
-                    placeholder='Confirm Password'
-                    fontSize={14}
-                    type='password'
-                    value={inputs.confirmPassword}
-                    onChange={(e) => setInputs({...inputs,confirmPassword:e.target.value})}
-                    />
-                ) : null}
-
-                <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14} onClick={handleAuth}>
-                    {isLogin? "Login" : "Sign up"}
-                </Button>
+                {isLogin ? <Login/> : <Signup/>}
 
                 {/*Google Login*/}
                 <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
@@ -59,10 +22,8 @@ const AuthForm = () => {
                     <Box flex={2} h={"1px"} bg={"gray.400"}/>
                 </Flex>
 
-                <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
-                    <Image src='/google.png' w={5} alt='Google logo'/>
-                    <Text mx={2} color={"blue.500"}>Login with Google</Text>
-                </Flex>
+                <GoogleAuth/>
+
             </VStack>
         </Box>
 
---
+++ src/components/AuthForm/Login.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Input, Button, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
+const Login = () => {
+    const [inputs, setInputs] = useState({
+        email:'',
+        password:'',
+    });
+
+    const [showPassword, setShowPassword] = useState(false);
+  return (
+    <>
+        <Input
+            placeholder='Email'
+            fontSize={14}
+            type='email'
+            value={inputs.email}
+            size={"sm"}
+            onChange={(e) => setInputs({...inputs,email:e.target.value})}
+        />
+        <InputGroup>
+            <Input
+                placeholder='Password'
+                fontSize={14}
+                type={showPassword ? "text" : "password"}
+                value={inputs.password}
+                size={"sm"}
+                onChange={(e) => setInputs({...inputs,password:e.target.value})}
+            />
+
+            <InputRightElement h={"full"}>
+                <Button variant={"ghost"} size={"sm"} onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? <ViewIcon/> : <ViewOffIcon/>}
+                </Button>
+            </InputRightElement>
+        </InputGroup>
+
+        <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14} >
+            Log in
+        </Button>
+    </>
+  )
+}
+
+export default Login
