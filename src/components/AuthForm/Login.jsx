9311aec5b548273a229c1781fb81d3c6317deb4e
manuelysf <158148948+manuelysf@users.noreply.github.com>
2024-10-16 15:18:08 +0200

    Login erfolgreich implementiert

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Input, Button, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { Input, Button, InputGroup, InputRightElement, Alert, AlertIcon } from '@chakra-ui/react'
 import { useState } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import useLogin from '../../hooks/useLogin';
 
 const Login = () => {
     const [inputs, setInputs] = useState({
@@ -10,6 +11,8 @@ const Login = () => {
     });
 
     const [showPassword, setShowPassword] = useState(false);
+
+    const { loading, error, login } = useLogin();
   return (
     <>
         <Input
@@ -18,7 +21,7 @@ const Login = () => {
             type='email'
             value={inputs.email}
             size={"sm"}
-            onChange={(e) => setInputs({...inputs,email:e.target.value})}
+            onChange={(e) => setInputs({...inputs,email: e.target.value})}
         />
         <InputGroup>
             <Input
@@ -27,7 +30,7 @@ const Login = () => {
                 type={showPassword ? "text" : "password"}
                 value={inputs.password}
                 size={"sm"}
-                onChange={(e) => setInputs({...inputs,password:e.target.value})}
+                onChange={(e) => setInputs({...inputs,password: e.target.value})}
             />
 
             <InputRightElement h={"full"}>
@@ -37,7 +40,16 @@ const Login = () => {
             </InputRightElement>
         </InputGroup>
 
-        <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14} >
+        {error && (
+            <Alert status='error' fontSize={13} p={2} borderRadius={4}>
+                <AlertIcon fontSize={12}/>
+                {error.message}
+            </Alert>
+        )}
+
+        <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}
+            isLoading={loading}
+            onClick={() => login(inputs)} >
             Log in
         </Button>
     </>
