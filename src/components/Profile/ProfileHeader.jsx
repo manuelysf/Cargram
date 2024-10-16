c3b3739d71eafa4717c2c9a55b2a68493733bb3f
manuelysf <158148948+manuelysf@users.noreply.github.com>
2024-10-16 20:51:50 +0200

    Preview hook für posts später/edit profile fertig

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,13 +1,15 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from '@chakra-ui/react'
+import { Avatar, AvatarGroup, Button, Flex, Text, useDisclosure, VStack } from '@chakra-ui/react'
 import React from 'react'
 import useUserProfileStore from '../../store/userProfileStore'
 import useAuthStore from '../../store/authStore';
+import EditProfile from './EditProfile';
 
 const ProfileHeader = () => {
     const {userProfile} = useUserProfileStore();
     const authUser = useAuthStore(state => state.user);
     const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
     const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+    const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Flex gap={{base:4,sm:10}} py={10} direction={{base:"column",sm:"row"}}>
         <AvatarGroup
@@ -32,7 +34,8 @@ const ProfileHeader = () => {
                 {/* Btn nur angezeigt wenn eignes Profil und angemeldet ist */}
                 {visitingOwnProfileAndAuth && (
                     <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-                        <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}>
+                        <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}
+                            onClick={onOpen}>
                             Edit Profile
                         </Button>
                     </Flex>
@@ -65,6 +68,7 @@ const ProfileHeader = () => {
             </Flex>
                 <Text fontSize={"sm"}>{userProfile.bio}</Text>
         </VStack>
+        {isOpen && <EditProfile isOpen={isOpen} onClose={onClose}/>}
     </Flex>
   )
 }
